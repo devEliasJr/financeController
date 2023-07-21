c5f4ee649a55a38fdd53f4ca67104f4acf9b98c3
devEliasJr <eliasengmecanica@gmail.com>
2023-07-21 16:37:46 -0300

    feat: Transactions Table

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import {
   HeaderContainer,
   HeaderContent,
   NewTransactionButton,
-} from "../../pages/Transactions/styles";
+} from "./styles";
 
 import Logoimg from '../../assets/Logo.svg'
 
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,10 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 type Props = {};
 
@@ -8,6 +13,30 @@ export function Transactions({}: Props) {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Salário Mensal</td>
+              <td>
+                <PriceHighlight variant="income">R$ 5.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>23/07/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Curso de Ingles</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 300,00</PriceHighlight>
+              </td>
+
+              <td>Venda</td>
+              <td>23/07/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
