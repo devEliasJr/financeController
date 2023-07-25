dd2e6778dfc96048342dc4b343af4cbf888f469a
devEliasJr <eliasengmecanica@gmail.com>
2023-07-25 14:29:45 -0300

    refactor: Isolating summary calculations in the custom hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/transactionsContext";
 import { SummaryCard, SummaryContainer } from "./styles";
 import {
   ArrowCircleUp,
@@ -8,25 +6,12 @@ import {
 } from "@phosphor-icons/react";
 import { priceFormatter } from "../../utils/formater";
 
+import { useSummary } from "../../hooks/useSummary";
+
 type Props = {};
 
 export function Summary({}: Props) {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transactions) => {
-      if (transactions.type === "income") {
-        acc.income += transactions.price;
-        acc.total += transactions.price
-      } else {
-        acc.outcome += transactions.price;
-        acc.total -= transactions.price
-      }
-
-      return acc;
-    },
-    { income: 0, outcome: 0, total: 0 }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
